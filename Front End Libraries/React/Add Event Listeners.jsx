160557044a63c477ad3d8cb3149ca2d7e4ad965f
Nguyễn Tùng Lâm <ntlisme@hotmail.com>
2018-12-21 11:25:07 +0700

    add new files

--- Front End Libraries/React/Add Event Listeners.jsx
+++ Front End Libraries/React/Add Event Listeners.jsx
@@ -2,17 +2,17 @@ class MyComponent extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        message: ''
+        message: ""
       };
       this.handleEnter = this.handleEnter.bind(this);
       this.handleKeyPress = this.handleKeyPress.bind(this);
     }
     // change code below this line
     componentDidMount() {
-      document.addEventListener("keydown",this.handleKeyPress)
+        document.addEventListener("keydown", this.handleKeyPress);
     }
     componentWillUnmount() {
-      document.removeEventListener("keydown",this.handleKeyPress)
+        document.removeEventListener("keydown", this.handleKeyPress);
     }
     // change code above this line
     handleEnter() {
